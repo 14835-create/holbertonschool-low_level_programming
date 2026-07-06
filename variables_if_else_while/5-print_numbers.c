998d76856a6609fa282eb0dad52bc3caa74d53b7
14835-create <14835@holbertonstudents.com>
2026-07-06 05:49:16 +0000

    5-print_numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints single digit numbers base 10 from 0
+ * main - Prints all single digit numbers of base 10 starting from 0.
  *
- * Reutnr: Always 0.
+ * Return: Always 0.
  */
 int main(void)
 {
