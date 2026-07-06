3d976a0adb8caf7b1ae43d03f31cb044f913a2dd
14835-create <14835@holbertonstudents.com>
2026-07-06 06:46:22 +0000

    9-print_comb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,17 +8,17 @@
  */
 int main(void)
 {
-	int num;
+    int n;
 
-	for (num = '0'; num <= '9'; num++)
-	{
-		putchar((num % 10) + '0');
-		if (num != '9')
-		putchar(',');
-		putchar(' ');
-	}
-
-	putchar('\n');
-
-	return (0);
+    for (n = 0; n <= 9; n++)
+    {
+        putchar(n + '0');
+        if (n != 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    putchar('\n');
+    return 0;
 }
