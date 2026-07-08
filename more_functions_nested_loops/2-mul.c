045516919c6d8bcc06281c2d6b19c6c264a13d07
14835-create <14835@holbertonstudents.com>
2026-07-08 01:43:53 +0000

    2-mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers
+ * @a: character check
+ * @b: character check
+ *
+ * Return: a and b result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- more_functions_nested_loops/README.md
+++ more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 0-isupper.c: checks for suppercase character c
 1-isdigit.c: checks for digit 0-9
+2-mul.c: multiplies two integers a b
