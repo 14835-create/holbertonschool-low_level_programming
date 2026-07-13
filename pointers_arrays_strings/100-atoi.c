f3781bb8286a7eff7c025e0ef208a470df6e59f5
14835-create <14835@holbertonstudents.com>
2026-07-13 06:05:49 +0000

    help

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -19,8 +19,17 @@ int _atoi(char *s)
 			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
+			int digit = s[i] - '0';
+
+			if (result > (2147483647 - digit) / 10)
+			{
+				if (sign == -1)
+					return (-2147483648);
+				return (2147483647);
+			}
+
+			result = result * 10 + digit;
 			started = 1;
-			result = result * 10 + (s[i] - '0');
 		}
 		else if (started)
 			break;
