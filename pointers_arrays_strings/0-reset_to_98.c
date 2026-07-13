01a8194552fed26c440352d7d64e73cbc72fc4c2
14835-create <14835@holbertonstudents.com>
2026-07-13 01:41:56 +0000

    reset_to_98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value of int pointed by n - 98
+ * @n: pointer to an int
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0-rest\_to\_98.c: functions that takes a pointer to an int as parameter and updates the value it point to 98
