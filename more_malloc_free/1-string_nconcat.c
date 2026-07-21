3804957a8e1ade46a509714a05aad9e19ccd9eb9
14835-create <14835@holbertonstudents.com>
2026-07-21 01:15:16 +0000

    concat

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes s2 to concatenate
+ *
+ * Return: funtion fails - NULL
+ * otherwise pointer to concatenated space in mem
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat;
+	unsigned int length, i;
+
+	length = n;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s1 = "";
+
+	for (i = 0; s1[i]; i++)
+		length++;
+
+	concat = malloc(sizeof(char) * (length + 1));
+
+	if (concat == NULL)
+		return (NULL);
+
+	length = 0;
+
+	for (i = 0; s1[i]; i++)
+		concat[length++] = s1[i];
+
+	for (i = 0; s2[i] && i < n; i++)
+		concat[length++] = s2[i];
+
+	concat[length] = '\0';
+
+	return (concat);
+}
--- more_malloc_free/README.md
+++ more_malloc_free/README.md
@@ -5,4 +5,6 @@ if statement - malloc fails, returns to NULL
 exit(98) - instead of returning NULL, terminates entire program
 return (pointer) - if allocation succeeds, return pointer
 
-
+1-string\_nconcat.c:
+concatenates two strings.
+If either s1, s2 NULL, trated as empty string. Added length to s1, counts characters in s1 and adds them to length. length = length(s1) + n - total number of characters to store. Allocated memory +1 for the NULL terminator. Length becomes position inside new string with length = 0. Copied every character of s1 into concat, moving write position forward. Program copies n bytes of s2. Loop stops when reaching end of s1 or the bytes of n have been copied. Returns the string (concat). Output prints characters Best School.
