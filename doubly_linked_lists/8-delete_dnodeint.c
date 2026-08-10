da2c7e4c0f24625c31fb33809e04fac791cb1553
14835-create <14835@holbertonstudents.com>
2026-08-10 12:40:19 +0000

    help

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -26,7 +26,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			(*head)->prev = NULL;
 
 		free(tmp);
-		return (-1);
+		return (1);
 	}
 
 	while (tmp != NULL && i < index)
