d11568ac4bda9512faad98f018d5748c5a9a48cd
14835-create <14835@holbertonstudents.com>
2026-08-12 03:29:44 +0000

    .

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -9,14 +9,14 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *node, *tmp;
-	unsigned long int;
+	unsigned long int i;
 
 	if (ht == NULL)
 		return;
 	
 	for (i = 0; i < ht->size; i++)
 	{
-		while = ht->array[i];
+		node = ht->array[i];
 		while (node != NULL)
 		{
 			tmp = node->next;
